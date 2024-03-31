08bad13d612b2eca391b6d680e8f98c8c938d320
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-03-31 20:09:02 -0300

    feat: add fake repo

--- repository.py
+++ repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 from model import Batch
 
@@ -11,3 +12,16 @@ class AbstractRepository(ABC):
     @abstractmethod
     def get(self, reference) -> Batch:
         raise NotImplementedError
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches: List[Batch]) -> None:
+        self._batches = set(batches)
+    
+    def add(self, batch: Batch):
+        self._batches.add(batch)
+    
+    def get(self, reference) -> Batch:
+        return next(b for b in self._batches if b.reference == reference)
+    
+    def list(self) -> List[Batch]:
+        return list(self._batches)
