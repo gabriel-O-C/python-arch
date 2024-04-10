99895b23fbf45fd1e763bbac5052d445f295cf41
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-04-10 15:42:22 -0300

    test: service logic

---
+++ test_services.py
@@ -0,0 +1,53 @@
+import pytest
+
+import model
+import repository
+import services
+
+
+class FakeSession():
+  commited = False
+
+  def commit(self):
+    self.commited = True
+
+
+
+def test_commits():
+  line = model.OrderLine('o1', 'COMPLICATED-LAMP', 10)
+
+  batch = model.Batch('b1', 'COMPLICATED-LAMP', 100, None)
+
+  repo =  repository.FakeRepository([batch])
+
+  session = FakeSession()
+
+
+  services.allocate(line, repo, session)
+
+  assert session.commited is True
+
+def test_returns_allocations():
+
+  line = model.OrderLine('o1', 'COMPLICATED-LAMP', 10)
+
+  batch = model.Batch('b1', 'COMPLICATED-LAMP', 100, None)
+
+  repo =  repository.FakeRepository([batch])
+
+  result = services.allocate(line, repo, FakeSession())
+
+
+  assert result == 'b1'
+
+
+
+def test_error_for_invalid_sku():
+  line = model.OrderLine('o1', 'NONEXISTENT-LAMP', 10)
+
+  batch = model.Batch('b1', 'COMPLICATED-LAMP', 100, None)
+
+  repo =  repository.FakeRepository([batch])
+
+  with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENT-LAMP"):
+    services.allocate(line, repo, FakeSession())
