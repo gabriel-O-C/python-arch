2ca2dcf71c922d7e638716706bc65cfc2b52516d
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-04-09 22:21:37 -0300

    test: tests api

--- repository.py
+++ repository.py
@@ -13,15 +13,30 @@ class AbstractRepository(ABC):
     def get(self, reference) -> Batch:
         raise NotImplementedError
 
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
+
+
 class FakeRepository(AbstractRepository):
     def __init__(self, batches: List[Batch]) -> None:
         self._batches = set(batches)
-    
+
     def add(self, batch: Batch):
         self._batches.add(batch)
-    
+
     def get(self, reference) -> Batch:
         return next(b for b in self._batches if b.reference == reference)
-    
+
     def list(self) -> List[Batch]:
         return list(self._batches)
